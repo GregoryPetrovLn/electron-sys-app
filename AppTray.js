5da9d547e306428f019e285ce1fa76ac7240f758
Gregory Petrov <grishap96123@gmail.com>
2022-08-06 18:59:22 +0300

    added class AppTray

---
+++ AppTray.js
@@ -0,0 +1,32 @@
+const { app, Menu, Tray } = require('electron');
+
+class AppTray extends Tray {
+  constructor(icon, mainWindow) {
+    super(icon);
+    this.mainWindow = mainWindow;
+
+    this.setToolTip('SysTop');
+
+    this.on('click', () => {
+      if (this.mainWindow.isVisible()) {
+        this.mainWindow.hide();
+      } else {
+        this.mainWindow.show();
+      }
+    });
+
+    this.on('right-click', () => {
+      const contextMenu = Menu.buildFromTemplate([
+        {
+          label: 'Quit',
+          click: () => {
+            (app.isQuitting = true), app.quit();
+          },
+        },
+      ]);
+      this.popUpContextMenu(contextMenu);
+    });
+  }
+}
+
+module.exports = AppTray;
